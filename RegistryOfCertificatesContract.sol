9056267e06dd4e1d570532c9b426de7ad12141d9
Denis Putnam <denisputnam@verizon.net>
2020-01-21 18:16:56 -0500

    Created RegistryOfCertificatesContract.sol

---
+++ RegistryOfCertificatesContract.sol
@@ -0,0 +1,25 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract RegistryOfCertificates {
+    mapping(string => bool) certificates;
+    address private owner;
+    event CertificateEvent(
+        address owner,
+        string hash,
+        bool flag
+    );
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function add(string memory hash) public {
+        require(msg.sender == owner, "You must be the owner to add a certificate.");
+        certificates[hash] = true;
+        emit CertificateEvent(owner, hash, true);
+    }
+
+    function verify(string memory hash) public view returns(string memory, bool) {
+        return(hash, certificates[hash]);
+    }
+}
