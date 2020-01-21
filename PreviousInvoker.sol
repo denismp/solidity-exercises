7d6b7ee0bad76ad47b5480721bf4fd5a986bf048
Denis Putnam <denisputnam@verizon.net>
2020-01-21 13:54:10 -0500

    Created PreviousInvoker.sol

---
+++ PreviousInvoker.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract PreviousInvoker {
+    address private lastInvoker;
+    event Invoked(
+        bool flag,
+        address lastInvoker
+    );
+
+    function getLastInvoker() public returns(bool, address) {
+        address previousInvoker = lastInvoker;
+        bool flag = false;
+        if( lastInvoker != address(0x0)) {
+            flag = true;
+        }
+        lastInvoker = msg.sender;
+        emit Invoked(flag, previousInvoker);
+        return (flag, previousInvoker);
+    }
+}
