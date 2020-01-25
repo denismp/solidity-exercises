794bb258777f77061a99596ab7ba2b55cad02997
Denis Putnam <denisputnam@verizon.net>
2020-01-25 18:12:36 -0500

    Basically got it working.  Uses massive amount of gas.

--- EarthContract.sol
+++ EarthContract.sol
@@ -1,9 +1,10 @@
 pragma solidity >=0.4.22 <0.6.2;
-//pragma experimental ABIEncoderV2; // This is experimental.  don't use in main net.
+//import "github.com/Arachnid/solidity-stringutils/strings.sol";
+pragma experimental ABIEncoderV2; // This is experimental.  don't use in main net.
 // https://blog.ethereum.org/2019/03/26/solidity-optimizer-and-abiencoderv2-bug/
 
 contract EarthContract {
-    string[7] private continents = ["Asia", "Afica", "Antartica", "Australia", "Europe", "North America", "South America"];
+    string[] private continents = ["Asia", "Afica", "Antartica", "Australia", "Europe", "North America", "South America"];
     //enum Continents {Asia, Africa, Antartica, Australia, NortAmerica, SouthAmerica}
     address private owner;
     struct Country {
@@ -13,6 +14,59 @@ contract EarthContract {
     }
     Country private countryRec;
     Country[] private countryArr;
+    string[] private europeanCountries =
+    [
+        "Albania",
+        "Andorra",
+        "Armenia",
+        "Austria",
+        "Azerbaijan",
+        "Belarus",
+        "Belgium",
+        "Bosnia",
+        "Croatia",
+        "Cyprus",
+        "Czechia",
+        "Denmark",
+        "Estonia",
+        "Finland",
+        "France",
+        "Georgia",
+        "Germany",
+        "Greece",
+        "Hungary",
+        "Iceland",
+        "Ireland",
+        "Italy",
+        "Kazzakhstan",
+        "Kosovo",
+        "Latvia",
+        "Liechtenstein",
+        "Lithuania",
+        "Luxembourg",
+        "Malta",
+        "Modova",
+        "Monaco",
+        "Montenegro",
+        "Netherlands",
+        "North Macedonia",
+        "Norway",
+        "Poland",
+        "Portugal",
+        "Romania",
+        "Russia",
+        "San Marino",
+        "Serbia",
+        "Slovakia",
+        "Slovenia",
+        "Spain",
+        "Sweden",
+        "Switzerland",
+        "Turkey",
+        "Ukraine",
+        "United Kingdom",
+        "Vatican City"
+    ];
 
     mapping(string => string) private europeanCapitals;
     mapping(string => Country[]) private continentsMap; // key is the continent.
@@ -71,6 +125,7 @@ contract EarthContract {
         europeanCapitals["Vatican City"] = "Vatican City";
     }
 
+
     function add(string memory country, string memory continent, uint population) public {
         require(bytes(europeanCapitals[country]).length != 0, "country is not a European country");
         countryRec.country = country;
@@ -79,13 +134,40 @@ contract EarthContract {
         continentsMap[continent].push(countryRec);
     }
 
-    function getEuropeanCountries() public view returns(string memory) {
+    function getEuropeanCountriesOld() public view returns(string memory) {
         string memory rString;
         Country[] memory eCountries = continentsMap["Europe"];
         for(uint i = 0; i < eCountries.length; i++) {
-            rString = strConcat(eCountries[i].country, "i");
+            rString = strConcat(rString,eCountries[i].country);
+            rString = strConcat(rString, ",");
         }
         return rString;
+    }
+
+    // function getAllEuropeanCountriesOld() public view returns(string memory) {
+    //     string memory rString;
+    //     for(uint i = 0; i < europeanCountries.length; i++) {
+    //         rString = strConcat(rString,europeanCountries[i]);
+    //         rString = strConcat(rString, ",");
+    //     }
+    //     return rString;
+    // }
+
+    function getAllEuropeanCountries() public view returns(string[] memory) {
+        return europeanCountries;
+    }
+
+    // function getContinentsOld() public view returns(string memory) {
+    //     string memory rString;
+    //     for(uint i = 0; i < continents.length; i++) {
+    //         rString = strConcat(rString,continents[i]);
+    //         rString = strConcat(rString,",");
+    //     }
+    //     return rString;
+    // }
+
+    function getContinents() public view returns(string[] memory) {
+        return continents;
     }
 
     function addCapital(string memory capital, string memory country) public {
@@ -93,6 +175,25 @@ contract EarthContract {
         for(uint i = 0; i < continentsMap["Europe"].length; i++) {
             if(hashCompareWithLengthCheck(continentsMap["Europe"][i].country,country)) {
                 continentsMap["Europe"][i].capital = capital;
+            }
+        }
+    }
+
+    function getCapital(string memory country) public view returns(string memory) {
+        require(bytes(europeanCapitals[country]).length != 0, "country is not a European country");
+        for(uint i = 0; i < continentsMap["Europe"].length; i++) {
+            if(hashCompareWithLengthCheck(continentsMap["Europe"][i].country,country)) {
+                return continentsMap["Europe"][i].capital;
+            }
+        }
+    }
+
+    function deleteCapital(string memory capital, string memory country) public {
+        require(bytes(europeanCapitals[country]).length != 0, "country is not a European country");
+        for(uint i = 0; i < continentsMap["Europe"].length; i++) {
+            if(hashCompareWithLengthCheck(continentsMap["Europe"][i].country,country)) {
+                require(hashCompareWithLengthCheck(continentsMap["Europe"][i].capital,capital), "Given capital is not the capital of the given country.");
+                delete continentsMap["Europe"][i].capital;
             }
         }
     }
@@ -140,6 +241,4 @@ contract EarthContract {
         for (uint i = 0; i < _bb.length; i++) bab[k++] = _bb[i];
         return string(bab);
     }
-
-
 }
